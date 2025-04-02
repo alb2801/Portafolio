a97f9601145e3a0d0be5e05b7459542c768a7373
alb2801 <alberthpulgarin@gmail.com>
2025-04-02 18:04:12 -0500

    Inicio de experiencia y inclucion de simple icons

--- src/app/pages/education/education.component.ts
+++ src/app/pages/education/education.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { ViewportScroller } from '@angular/common';
+import { filter } from 'rxjs';
 import { ContactModalComponent } from "../../components/contact-modal/contact-modal.component";
 
 @Component({
@@ -8,5 +11,11 @@ import { ContactModalComponent } from "../../components/contact-modal/contact-mo
   styleUrl: './education.component.css'
 })
 export class EducationComponent {
-  
+  constructor(private router: Router, private viewportScroller: ViewportScroller) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      this.viewportScroller.scrollToPosition([0, 0]);
+    })
+  }
 }
